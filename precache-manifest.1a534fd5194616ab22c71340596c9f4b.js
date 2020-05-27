1259e38693f12d3c5609bdc86689748f63e66300
shubh <ror.shubh@gmail.com>
2020-05-27 17:07:56 +0530

    Updates

---
+++ precache-manifest.1a534fd5194616ab22c71340596c9f4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa37cff3bf3bb64a1fbaef888fbdad87",
+    "url": "/plivo-assignment/index.html"
+  },
+  {
+    "revision": "f369add99c7fa25f6f12",
+    "url": "/plivo-assignment/static/css/main.45b217cd.chunk.css"
+  },
+  {
+    "revision": "93731bb1b87c9cf0fa70",
+    "url": "/plivo-assignment/static/js/2.fb02b890.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/plivo-assignment/static/js/2.fb02b890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f369add99c7fa25f6f12",
+    "url": "/plivo-assignment/static/js/main.7b2c40fb.chunk.js"
+  },
+  {
+    "revision": "43549e721e0d853a358a",
+    "url": "/plivo-assignment/static/js/runtime-main.f3b7baf8.js"
+  }
+]);
